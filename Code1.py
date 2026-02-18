534f66d3c641796fd0e49148c5f4eb6b8690963a
diya-mun <diya.mundra2025@bitsdesign.edu.in>
2026-02-18 13:59:11 +0530

    Add wave control loop for GPIO pins
    Implement wave control logic for pins

--- Code1.py
+++ Code1.py
@@ -1,1 +1,23 @@
+from machine import time
+import time
+
+wave = [[1.0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+in1 = Pin(14,PIN.OUT)
+in2 = Pin(25,PIN.OUT)
+in3 = Pin(26,PIN.OUT)
+in4 = Pin(27,PIN.OUT)
+
+count = 0
+
+while True :
+     for i in wave:
+         in1.value(i[1])
+         in2.value(i[2])
+         in3.value(i[3])
+         in4.value(i[4])
+        
+         count = count +4
+        
+        
 
