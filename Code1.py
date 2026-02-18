a1fb781c2bea34fe3cca4958f86297db0f98c499
diya-mun <diya.mundra2025@bitsdesign.edu.in>
2026-02-18 13:31:42 +0530

    code1
    Implement a wave pattern using GPIO pins for output.

--- Code1.py
+++ Code1.py
@@ -1,1 +1,36 @@
+from machine import Pin
+
+
+wave = [[1.0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+in1 = Pin(14,PIN.OUT)
+in2 = Pin(25,PIN.OUT)
+in3 = Pin(26,PIN.OUT)
+in4 = Pin(27,PIN.OUT)
+
+while True :
+    for i in wave:
+        in1.value(i[1])
+        in2.value(i[0])
+        in3.value(i[0])
+        in4.value(i[0])
+        
+        in1.value(i[0])
+        in2.value(i[1])
+        in3.value(i[0])
+        in4.value(i[0])
+        
+        
+        
+        in1.value(i[0])
+        in2.value(i[0])
+        in3.value(i[1])
+        in4.value(i[0])
+        
+   
+        
+        
+        
+        
+        
 
